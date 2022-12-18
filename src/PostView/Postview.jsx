15f4ed5a3b19d48ebb1d80064c66d57d8fc4ad8d
Abhishek Kumar <67044381+abhi751@users.noreply.github.com>
2022-12-18 20:35:01 +0530

    new change add once

--- src/PostView/Postview.jsx
+++ src/PostView/Postview.jsx
@@ -6,7 +6,7 @@ import Post from './Post';
 import Form from './Form';
 import axios from 'axios';
 import Loading from './Loading.jsx';
-const apiURL = 'https://insta-backend-fo20.onrender.com/post';
+const apiURL = 'https://backend-work-qo6c.onrender.com';
 const Postview = () => {
     let [data, setdata] = useState([])
     let [postAdded, setPostAdded] = useState(0);
